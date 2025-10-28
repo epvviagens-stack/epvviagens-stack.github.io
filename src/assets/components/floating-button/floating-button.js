636c8932fcb9e503ce501628785f5afadac0c7a7
leonadofalango <leo_falango@hotmail.com>
2025-10-27 23:25:14 -0300

    fix absolute url to domain

--- src/assets/components/floating-button/floating-button.js
+++ src/assets/components/floating-button/floating-button.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch(`https://epraviajar.com.br/src/assets/components/floating-button/floating-button.html`)
+    fetch(`/assets/components/floating-button/floating-button.html`)
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML('beforeend', data);
